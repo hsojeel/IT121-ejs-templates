285e18585a1e6ead454d3ea6edd20ad90ec912fa
Josh Lee <103002260+hsojeel@users.noreply.github.com>
2022-08-15 19:41:06 +0000

    finish

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine','ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = 'Our Home Page';
+  res.render('pages/index',{title:title});
+});
+
+//about-us
+app.get('/about-us', (req, res) => {
+  var title = 'Our About Us Page';
+  res.render('pages/about-us',{title:title});
+});
+
+
+app.get('/nuggets', (req, res) => {
+  var title = 'Denver Nuggets';
+  res.render('pages/nuggets',{title:title});
+});
+
+app.get('/suns', (req, res) => {
+  var title = 'Phoenix Suns';
+  res.render('pages/suns',{title:title});
+});
+
+app.get('/hornets', (req, res) => {
+  var title = 'Charlotte Hornets';
+  res.render('pages/hornets',{title:title});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
